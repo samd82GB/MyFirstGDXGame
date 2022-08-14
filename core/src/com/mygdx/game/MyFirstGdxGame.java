8e1ef749ad07198ae54be36751f80ce5d9c4f587
samd82GB <samd82@yandex.ru>
2022-08-14 21:53:10 +0500

    Выполнено домашнее задание к уроку 1. Привязка мышки к центру картинки и подсчёт кликов по левой кнопке в названии.

--- core/src/com/mygdx/game/MyFirstGdxGame.java
+++ core/src/com/mygdx/game/MyFirstGdxGame.java
@@ -1,6 +1,8 @@
 package com.mygdx.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
@@ -8,18 +10,30 @@ import com.badlogic.gdx.utils.ScreenUtils;
 public class MyFirstGdxGame extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
+	int click;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		img = new Texture("ReStart_Logo.jpg");
 	}
 
 	@Override
 	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
+		float x = Gdx.input.getX() - img.getWidth()/2;
+		float y = Gdx.graphics.getHeight() - Gdx.input.getY() - img.getHeight()/2;
+
+		if (Gdx.input.isButtonJustPressed(Input.Buttons.LEFT))
+			click++;
+
+		Gdx.graphics.setTitle("Мышка нажата "+click + " раз!");
+
+
+
+
+		ScreenUtils.clear(1, 1, 1, 1);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.draw(img, x,y);
 		batch.end();
 	}
 	
