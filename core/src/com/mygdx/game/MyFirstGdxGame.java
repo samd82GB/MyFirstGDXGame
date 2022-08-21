edf6d793dda51c951e7830ee984f4eb17c6d334e
samd82GB <samd82@yandex.ru>
2022-08-22 02:08:24 +0500

    Выполнено домашнее задание к уроку 2.

--- core/src/com/mygdx/game/MyFirstGdxGame.java
+++ core/src/com/mygdx/game/MyFirstGdxGame.java
@@ -4,42 +4,90 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 public class MyFirstGdxGame extends ApplicationAdapter {
 	SpriteBatch batch;
-	Texture img;
+//	Texture img;
 	int click;
-	
+	Anime anime;
+	int count=0;
+	boolean dir = false;
+
+
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("ReStart_Logo.jpg");
+		//img = new Texture("ReStart_Logo.jpg");
+		anime = new Anime("boldbaby.png", 9, 4, Animation.PlayMode.LOOP);
 	}
 
 	@Override
 	public void render () {
-		float x = Gdx.input.getX() - img.getWidth()/2;
-		float y = Gdx.graphics.getHeight() - Gdx.input.getY() - img.getHeight()/2;
 
-		if (Gdx.input.isButtonJustPressed(Input.Buttons.LEFT))
-			click++;
+		// Вводим данные по времени для анимации, равные времени между фреймами
+		anime.setTime(Gdx.graphics.getDeltaTime());
 
-		Gdx.graphics.setTitle("Мышка нажата "+click + " раз!");
+		//Координаты перемещения рисунка
+		//привязка мышки к центру рисунка
+//		float x = Gdx.input.getX() - anime.getFrame().getRegionWidth()/2;
+//		float y = Gdx.graphics.getHeight() - Gdx.input.getY() - anime.getFrame().getRegionHeight()/2;
+
+		//Привязать перемещение рисунка дельте по времени
+		int border = Gdx.graphics.getWidth()-anime.getFrame().getRegionWidth();
+
+
+ 		float x = 0;
+		float y = 0;//Gdx.graphics.getHeight() - anime.getFrame().getRegionHeight();
+
+		if (!dir) {
+			if (count<border) {
+				count++;
+
+			}
+			if (count>=border){
+				dir = true;
+//
+			}
+		}
+		if (dir) {
+			if (count<=border) {
+				count--;
+			}
+			if (count ==0) {
+				dir = false;
+
+			}
+		}
+		//если идём вправо и повёрнут, то поворачиваем ещё раз
+		if (!dir&&anime.getFrame().isFlipX()) {
+			anime.getFrame().flip(true, false);
+		}
+		//если идём влево и не повёрнут, то поворачиваем
+		if (dir&&!anime.getFrame().isFlipX()){
+			anime.getFrame().flip(true, false);
+		}
+
+//		if (Gdx.input.isButtonJustPressed(Input.Buttons.LEFT))
+//			click++;
+
+//		Gdx.graphics.setTitle("Мышка нажата "+click + " раз!");
 
 
 
 
 		ScreenUtils.clear(1, 1, 1, 1);
 		batch.begin();
-		batch.draw(img, x,y);
+		batch.draw(anime.getFrame(),x+count,y);
 		batch.end();
 	}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
+		anime.dispose();
 	}
 }
